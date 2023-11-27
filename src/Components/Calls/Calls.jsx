f3f6dfc6cfdbe3a6d820c44f2dc331cca76c682a
ARTALEAL <artaleal@yandex.ru>
2023-11-27 22:47:56 +0300

    header date and calls

--- src/Components/Calls/Calls.jsx
+++ src/Components/Calls/Calls.jsx
@@ -1,10 +1,62 @@
 import SideMenu from '../SideMenu/SideMenu';
 import './calls.css';
+import { getFormatDateForHeader } from '../../utils/utils';
 
 function Calls() {
   return (
     <main className="calls">
       <SideMenu />
+      <header className="calls-header">
+        <div className="calls-header__info-container">
+          <div className="calls-header__date">
+            {getFormatDateForHeader(new Date())}
+          </div>
+          <div className="calls-header__calls-info">
+            <div className="calls-header__calls-info-item">
+              <p className="calls-header__calls-info-item-text">
+                Новые звонки{' '}
+                <span
+                  className="calls-header__calls-info-item-text"
+                  data-color="green"
+                >
+                  20 из 30 шт
+                </span>
+              </p>
+              <div className="calls-header__calls-info-item-scale">
+                <hr data-color="green" /> <hr data-color="green-end" />
+              </div>
+            </div>
+            <div className="calls-header__calls-info-item">
+              <p className="calls-header__calls-info-item-text">
+                Качество разговоров{' '}
+                <span
+                  className="calls-header__calls-info-item-text"
+                  data-color="yellow"
+                >
+                  40%
+                </span>
+              </p>
+              <div className="calls-header__calls-info-item-scale">
+                <hr data-color="yellow" /> <hr data-color="yellow-end" />
+              </div>
+            </div>
+            <div className="calls-header__calls-info-item">
+              <p className="calls-header__calls-info-item-text">
+                Конверсия в заказ{' '}
+                <span
+                  className="calls-header__calls-info-item-text"
+                  data-color="red"
+                >
+                  67%
+                </span>
+              </p>
+              <div className="calls-header__calls-info-item-scale">
+                <hr data-color="red" /> <hr data-color="red-end" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
     </main>
   );
 }
---
+++ src/utils/utils.js
@@ -0,0 +1,8 @@
+export const getFormatDateForHeader = (date) => {
+  const str = date.toLocaleString('default', {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'short',
+  });
+  return str[0].toUpperCase() + str.slice(1);
+};
